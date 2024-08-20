c24874f47b1c01b53c872fc40b79cd289130546c
frerot <ntwalifrerotjacques@gmail.com>
2024-08-20 19:27:04 +0200

    add: finished the hero section with responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-import logo from './assets/logo.svg';
-import Image from 'next/image';
+import Hero from '#/components/Hero';
+
 export default function Home() {
  return (
-  <main className="flex items-center justify-center h-screen w-screen">
-   <div className="flex flex-col items-center justify-center px-2">
-    <Image src={logo} alt="whitepace" priority={true} />
-    <h1 className="text-[#4F9CF9] font-bold ">Comming soon...</h1>
-   </div>
+  <main>
+   <Hero />
   </main>
  );
 }
