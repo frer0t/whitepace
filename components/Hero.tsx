a123234e6973de4df27040e350fef86d4acef3cb
frerot <ntwalifrerotjacques@gmail.com>
2024-08-20 19:27:04 +0200

    add: finished the hero section with responsiveness

---
+++ components/Hero.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+import img1 from '../assets/1st img.png';
+import rightArrow from '../assets/right arrow icon.svg';
+const Hero = () => {
+ return (
+  <section
+   id="overview"
+   className="w-screen flex flex-col items-center justify-center gap-y-[100px] px-[16px] py-[100px] md:pt-[140px] md:pb-[100px] md:px-[20px] lg:h-screen lg:flex-row lg:px-[32px] lg:py-[140px] xl:px-[32px] xl:py-[100px] 2xl:px-[220px] 2xl:py-[140px]"
+  >
+   <div className="flex flex-col items-center lg:items-start gap-y-8 ">
+    <h1 className="text-center lg:text-start text-[36px] leading-[40px] md:text-[54px] md:leading-[60px] lg:text-[70px] lg:leading-[80px] font-bold text-blue">
+     Get More Done with whitepace
+    </h1>
+    <p className=" text-center lg:text-start text-[15px] sm:text-[18px] font-normal text-blue">
+     Project management software that enables your teams to collaborate, plan,
+     analyze and manage everyday tasks
+    </p>
+    <button className="flex items-center justify-center text-[12px] sm:text-[18px] text-white bg-blue rounded-lg px-[30px] py-[15px] lg:px-[20px] lg:py-[10px]">
+     Try Whitepace free
+     <Image src={rightArrow} alt="right arrow icon" className="w-[34px]" />
+    </button>
+   </div>
+   <Image
+    src={img1}
+    alt="toon image"
+    priority={true}
+    className="md:min-w-[550px] lg:min-w-[600px] xl:min-w-[800px] 2xl:min-w-[1000px]"
+   />
+  </section>
+ );
+};
+
+export default Hero;
