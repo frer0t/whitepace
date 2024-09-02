16211f76fe62dedcb9d32a851ba74972a89930e3
frérot <149209283+iamfrerot@users.noreply.github.com>
2024-09-02 14:25:41 +0200

    Nav and Hero Section
    * add: finished the hero section with responsiveness
    
    * feat:Navigation bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,28 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from '#/components/Navbar';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
  title: 'whitepace | Get more done',
  description:
   'Project management software that enables your teams to collaborate, plan, analyze and manage everyday tasks',
- icons: ['/bluelogoicon.svg'],
+ icons: ['/logoicon.svg'],
 };
 
 export default function RootLayout({
-  children,
+ children,
 }: Readonly<{
-  children: React.ReactNode;
+ children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+ return (
+  <html lang="en">
+   <body className={inter.className}>
+    <Navbar />
+    {children}
+   </body>
+  </html>
+ );
 }
