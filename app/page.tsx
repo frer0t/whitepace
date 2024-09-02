b28645b6b8a14d63641b94878a69a145ae91f82f
frérot <149209283+iamfrerot@users.noreply.github.com>
2024-09-02 14:26:20 +0200

    Revert "Nav and Hero Section"
    This reverts commit 16211f76fe62dedcb9d32a851ba74972a89930e3.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Hero from '#/components/Hero';
-
+import logo from './assets/logo.svg';
+import Image from 'next/image';
 export default function Home() {
  return (
-  <main>
-   <Hero />
+  <main className="flex items-center justify-center h-screen w-screen">
+   <div className="flex flex-col items-center justify-center px-2">
+    <Image src={logo} alt="whitepace" priority={true} />
+    <h1 className="text-[#4F9CF9] font-bold ">Comming soon...</h1>
+   </div>
   </main>
  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,29 +1,20 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
- content: [
-  './pages/**/*.{js,ts,jsx,tsx,mdx}',
-  './components/**/*.{js,ts,jsx,tsx,mdx}',
-  './app/**/*.{js,ts,jsx,tsx,mdx}',
- ],
- theme: {
-  extend: {
-   colors: {
-    darkblue: '#043873',
-    blue: '#4F9CF9',
-    lightyellow: '#FFE492',
-    lightblue: '#A7CEFC',
-    bluegray: '#212529',
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: { 
+    colors: {
+      darkblue: "#043873",
+      blue: "#4F9CF9",
+      lightyellow: "#FFE492",
+      lightblue: '#A7CEFC',
+      bluegray:'#212529'
+    }
    },
-   screens: {
-    '2xl': '1920px',
-    xl: '1440px',
-    lg: '1152px',
-    md: '768px',
-    sm: '320px',
-   },
-  },
- },
- plugins: [],
+  plugins: [],
 };
 export default config;
